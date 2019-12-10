2d1e5bae50d0d88acb97c9a95643a7837228b8a7
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2019-12-10 18:29:15 +0100

    Menu de création de personnage dev.

--- unitmenucreationpersonnage.pas
+++ unitmenucreationpersonnage.pas
@@ -1,9 +1,8 @@
-unit unitAffichage;
+unit unitMenuCreationPersonnage;
 
 {$codepage utf8}
 
 interface
-
 (*#########################################################*)
 (*                                                         *)
 (*   ###  #  #  #####  ####  ###   ####   ###   ##   ####  *)
@@ -14,7 +13,7 @@ interface
 (*                                                         *)
 (*#########################################################*)
 
-uses unitEcran, unitMenuInitiale, unitMenuCreationPersonnage;
+uses unitEcran, sysutils;
 
 
 
@@ -27,27 +26,31 @@ uses unitEcran, unitMenuInitiale, unitMenuCreationPersonnage;
     (*  #      ##   #  #   ##     #    #      #     #  #   ##     ##   *)
     (*                                                                 *)
     (*#################################################################*)
-
-{Initialise la taille et le cadre de la console}
-procedure initConsole();
-
 (*Attend une réponse de la part de l'utilisateur.*)
 function jeVeuxUneReponse() : String;
 
 
 
 
-(*Procedure qui modifie qF en fonction du choix de l'utilisateur, soit il ferme le jeu, soit il démare une nouvelle partie.*)
-procedure affMenuInitiale(var qF : String);
+(*Affiche le texte de demande du pseudo du personnage.*)
+procedure affTexteDemandeNomPersMCP();
 
-(*Procedure qui modifie qF en fonction du choix de l'utilisateur, soit il ferme le jeu, soit il démare une nouvelle partie.*)
-procedure affMenuCreationPersonnage(var qF : String);
+(*Affiche le pseudo de manière centrée.*)
+procedure centrerPseudoMCP(p : String);
+
+(*Affiche le texte des différentes races.*)
+procedure affRacesMCP();
+
+(*Retourne la race que le joueur désire utiliser.*)
+function choixRaceMCP() : Integer;
+
+(*Demande au joueur s'il veut lancer et retourne sa réponse (FALSE si oui et vice versa).*)
+function confirmationMCP() : Boolean;
 
 
 
 
 implementation
-
 (*#########################################################################################*)
 (*                                                                                         *)
 (*   ###  #   #  ####  #     ####  #   #  ####  #  #  #####   ###  #####  ###   ###  #  #  *)
@@ -57,14 +60,6 @@ implementation
 (*   ###  #   #  #     ####  ####  #   #  ####  #  #    #    #  #    #    ###   ##   #  #  *)
 (*                                                                                         *)
 (*#########################################################################################*)
-
-{Initialise la taille et le cadre de la console}
-procedure initConsole();
-begin
-  changerTailleConsole(200, 60);
-  effacerEcran;
-  dessinerCadreXY(2,1,196,58,double,15,0);
-end;
 
 (*Attend une réponse de la part de l'utilisateur.*)
 function jeVeuxUneReponse() : String;
@@ -77,59 +72,81 @@ end;
 
 
 
-
-
-(*Procedure qui modifie qF en fonction du choix de l'utilisateur, soit il ferme le jeu, soit il démare une nouvelle partie.*)
-procedure affMenuInitiale(var qF : String);
-var
-  rep : String;  // Variable qui vaut n ou k pour respectivement créer une nouvelle partie ou killer le jeu.
-
+(*Affiche le texte de demande du pseudo du personnage.*)
+procedure affTexteDemandeNomPersMCP();
 begin
-  initConsole();
-
-  affichageTextesMI();
-
-  repeat
-    centrerTexte('                  ', 49, 100);
-    centrerTexte('', 49, 100);
-    rep := jeVeuxUneReponse();
-  until (rep = '1') OR (rep = '2');
-
-  if rep = '1' then rep := 'n';
-  if rep = '2' then rep := 'exit';
-
-  qF := rep;
+  centrerTexte('Comment veux-tu qu''on t''appelle ?', 15, 100);
+  centrerTexte('', 16, 100);
 end;
 
-(*Procedure qui modifie qF en fonction du choix de l'utilisateur, soit il ferme le jeu, soit il démare une nouvelle partie.*)
-procedure affMenuCreationPersonnage(var qF : String);
+(*Affiche le pseudo de manière centrée.*)
+procedure centrerPseudoMCP(p : String);
+begin
+  centrerTexte('                                                       ', 16, 100);
+  centrerTexte(p, 16, 100);
+end;
+
+(*Affiche le texte des différentes races.*)
+procedure affRacesMCP();
+begin
+  centrerTexte('1/ Altmer', 25, 100);
+  centrerTexte('2/ Argonien', 26, 100);
+  centrerTexte('3/ Bosmer', 27, 100);
+  centrerTexte('4/ Breton', 28, 100);
+  centrerTexte('5/ Dunmer', 29, 100);
+  centrerTexte('6/ Imperial', 30, 100);
+  centrerTexte('7/ Khajit', 31, 100);
+  centrerTexte('8/ Nordique', 32, 100);
+  centrerTexte('9/ Orsimer', 33, 100);
+  centrerTexte('10/ Rougegarde', 34, 100);
+end;
+
+(*Retourne la race que le joueur désire utiliser.*)
+function choixRaceMCP() : Integer;
 var
-  ctn : Boolean;
-  nom : String;
-  race : Integer;
+  rep : String;
+  A, B, C, D, R, S : Boolean;
 
 begin
-  ctn := True;
+  repeat
+    centrerTexte('De quelle race est-tu ?', 36, 100);
+    centrerTexte('              ', 37, 100);
+    centrerTexte('', 37, 100);
+    rep := jeVeuxUneReponse();
 
-  while ctn do
-    begin
-      initConsole();
+    R := Length(rep) = 1;
+    S := Length(rep) = 2;
+    A := Ord('0') < Ord(rep[1]);
+    B := Ord(rep[1]) <= Ord('9');
+    C := rep[1] = '1';
+    D := rep[2] = '0';
 
-      // Demande du nom du joueur
-      affTexteDemandeNomPersMCP;
-      nom :=jeVeuxUneReponse();
+  until ( (R AND A AND B) OR (S AND C AND D) );
 
-      centrerPseudoMCP(nom);
+  choixRaceMCP := StrToInt(rep);
+end;
 
-      // Affiche les races
-      affRacesMCP;
+(*Demande au joueur s'il veut lancer et retourne sa réponse (FALSE si oui et vice versa).*)
+function confirmationMCP() : Boolean;
+var
+  rep : String;
+  res : Boolean;
 
-      // Vérifie et donne la race que l'utilisateur désire.
-      race := choixRaceMCP();
+begin
+  repeat
+    centrerTexte('Es-tu prêt à commencer la partie ? (oui/non)', 45, 100);
+    centrerTexte('', 46, 100);
+    rep := jeVeuxUneReponse();
+  until (rep <> 'oui') OR (rep <> 'non');
 
-      ctn := confirmationMCP();
-    end;
+  if rep = 'oui' then
+    res := FALSE
+  else
+    res := TRUE;
+
+  confirmationMCP := res;
 end;
 
 end.
+
 
